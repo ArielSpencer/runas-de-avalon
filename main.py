37a4db6e452e03a4175421fd046e02d8519e7421
Ariel Spencer <arielspencer.tech@gmail.com>
2025-03-14 23:07:54 -0300

    feat: translate variables and functions to English while preserving UI text in Portuguese

--- main.py
+++ main.py
@@ -1,99 +1,91 @@
 from random import randint
 
-lista_npcs = []
+npc_list = []
 
 player = {
-  "nome": "Player",
-  "level": 1,
-  "exp": 0,
-  "exp_max": 30,
-  "hp": 100,
-  "hp_max": 100,
-  "dano": 25,
+    "name": "Player",
+    "level": 1,
+    "exp": 0,
+    "exp_max": 30,
+    "hp": 100,
+    "hp_max": 100,
+    "damage": 25,
 }
 
-def criar_npc(level):
-  
-  novo_npc = {
-    "nome": f"NPC #{level}",
-    "level": level,
-    "dano": 5 * level,
-    "hp": 100 * level,
-    "hp_max": 100 * level,
-    "exp": 7 * level,
-  }
-  
-  return novo_npc
+def create_npc(level):
+    new_npc = {
+        "name": f"NPC #{level}",
+        "level": level,
+        "damage": 5 * level,
+        "hp": 100 * level,
+        "hp_max": 100 * level,
+        "exp": 7 * level,
+    }
+    return new_npc
 
-def gerar_npcs(n_npcs):
+def generate_npcs(n_npcs):
     for x in range(n_npcs):
-        novo_npc = criar_npc(x + 1)
-        lista_npcs.append(novo_npc)
-        
+        new_npc = create_npc(x + 1)
+        npc_list.append(new_npc)
 
-def exibir_npcs():
-    for npc in lista_npcs:
-        exibir_npc(npc)
-        
-def exibir_npc(npc):
+def display_npcs():
+    for npc in npc_list:
+        display_npc(npc)
+
+def display_npc(npc):
     print(
-          f"Nome: {npc['nome']} // Level: {npc['level']} // Dano: {npc['dano']} // HP: {npc['hp']} // Exp: {npc['exp']}"
+        f"Nome: {npc['name']} // Level: {npc['level']} // Dano: {npc['damage']} // HP: {npc['hp']} // Exp: {npc['exp']}"
     )
-    
-def exibir_player():
+
+def display_player():
     print(
-          f"Nome: {player['nome']} // Level: {player['level']} // Dano: {player['dano']} // HP: {player['hp']}/{player['hp_max']} // Exp: {player['exp']}/{player['exp_max']}"
+        f"Nome: {player['name']} // Level: {player['level']} // Dano: {player['damage']} // HP: {player['hp']}/{player['hp_max']} // Exp: {player['exp']}/{player['exp_max']}"
     )
-    
+
 def reset_player():
     player["hp"] = player["hp_max"]
-    
+
 def reset_npc(npc):
     npc["hp"] = npc["hp_max"]
-    
+
 def level_up():
     if player["exp"] >= player["exp_max"]:
         player["level"] += 1
         player["exp"] = 0
         print("Level UP!")
 
-def iniciar_batalha(npc):
+def start_battle(npc):
     while player["hp"] > 0 and npc["hp"] > 0:
-      atacar_npc(npc)
-      atacar_player(npc)
-      exibir_info_batalha(npc)
-      
+        attack_npc(npc)
+        attack_player(npc)
+        display_battle_info(npc)
+
     if player["hp"] > 0:
-      print(f"Player venceu! + {npc['exp']} de EXP")
-      player["exp"] += npc["exp"]
-      exibir_player()
+        print(f"Player venceu! + {npc['exp']} de EXP")
+        player["exp"] += npc["exp"]
+        display_player()
     else:
-      print(f"{npc['nome']} venceu!")
-    
+        print(f"{npc['name']} venceu!")
+
     level_up()
-      
+
     reset_player()
     reset_npc(npc)
 
-def atacar_npc(npc):
-    npc["hp"] -= player["dano"]
-    
-def atacar_player(npc):
-    player["hp"] -= npc["dano"]
+def attack_npc(npc):
+    npc["hp"] -= player["damage"]
 
-def exibir_info_batalha(npc):
+def attack_player(npc):
+    player["hp"] -= npc["damage"]
+
+def display_battle_info(npc):
     print(f"Player: {player['hp']}/{player['hp_max']}")
-    print(f"{npc['nome']}: {npc['hp']}/{npc['hp_max']}")
+    print(f"{npc['name']}: {npc['hp']}/{npc['hp_max']}")
     print("-----------------------------\n")
 
-gerar_npcs(3)
-# exibir_npcs()
+generate_npcs(3)
 
-npc_selecionado = lista_npcs[0]
-iniciar_batalha(npc_selecionado)
-iniciar_batalha(npc_selecionado)
-iniciar_batalha(npc_selecionado)
-iniciar_batalha(npc_selecionado)
-iniciar_batalha(npc_selecionado)
+selected_npc = npc_list[0]
+start_battle(selected_npc)
 
-exibir_player()
+display_player()
