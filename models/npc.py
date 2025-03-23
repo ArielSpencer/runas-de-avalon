d146e0c889af7e55f06696017bf11f49072c78e4
Ariel Spencer <arielspencer.tech@gmail.com>
2025-03-22 21:34:06 -0300

    refactor(npc): adjust attributes and level scaling logic

--- models/npc.py
+++ models/npc.py
@@ -2,16 +2,18 @@ def create_npc(level):
     return {
         "name": f"NPC #{level}",
         "level": level,
-        "damage": 5 * level,
-        "hp": 100 * level,
-        "hp_max": 100 * level,
-        "exp": 7 * level,
+        "damage": 4 * level,
+        "hp": 80 * level,
+        "hp_max": 80 * level,
+        "exp": 9 * level,
     }
 
 def generate_npcs(n_npcs):
     npc_list = []
     for x in range(n_npcs):
-        new_npc = create_npc(x + 1)
+        npc_level = (x // 2) + 1
+        new_npc = create_npc(npc_level)
+        new_npc["name"] = f"NPC #{x + 1}"
         npc_list.append(new_npc)
     return npc_list
 
