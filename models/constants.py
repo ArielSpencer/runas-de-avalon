0d51026a33b7cbab4990b7859e61d6b8fdda4739
Ariel Spencer <arielspencer.tech@gmail.com>
2025-03-22 21:27:02 -0300

    feat(models): add constants

---
+++ models/constants.py
@@ -0,0 +1,17 @@
+PLAYER_CLASSES = {
+    "Guerreiro": {
+        "hp_max": 120,
+        "damage": 20,
+        "description": "Forte e resistente, o Guerreiro tem mais HP mas causa menos dano."
+    },
+    "Mago": {
+        "hp_max": 80,
+        "damage": 35,
+        "description": "Frágil mas poderoso, o Mago causa mais dano mas tem menos HP."
+    },
+    "Arqueiro": {
+        "hp_max": 100,
+        "damage": 25,
+        "description": "Balanceado, o Arqueiro tem atributos medianos."
+    }
+}
